6d0414ff0e0ac7e1e7210b7571b8bcb41f8d9860
yadaveknath98@gmail.com <yadaveknath98@gmail.com>
2020-12-18 16:27:40 +0530

    invalid city fixed

--- front/views.py
+++ front/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import requests
+
+
 # Create your views here.
 def frontpage(request):
     city = request.GET.get('City')
     url = "http://api.weatherapi.com/v1/current.json?key=3db1f7bb90624f7c80f143933201612&q=" + str(city)
-    response = requests.get(url)
-    temp = response.json()['current']['temp_c']
-    city = response.json()['location']['name']
-    country = response.json()['location']['country']
-    return render(request,'front/index.html',{'temp':temp,'city':city,'country':country})
+    try:
+        response = requests.get(url)
+        temp = response.json()['current']['temp_c']
+        city = response.json()['location']['name']
+        country = response.json()['location']['country']
+    except KeyError:
+        city = None
+        temp = None
+        country = None
+    return render(request, 'front/index.html', {'temp': temp, 'city': city, 'country': country})
